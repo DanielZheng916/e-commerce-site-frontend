3ad7c2094c33e55fd30e26d0a79206bd9c8f400a
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-06-30 14:35:35 -0400

    Remove MUI, create a basic product page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 "use client"
 
-import MenuAppBar from "@/components/MenuAppBar"
-
 
 
 export default function Home() {
   return (
-    <MenuAppBar/>
+    <>
+      <div className="flex justify-center py-3 border-b border-gray-200 bg-slate-50">
+        <p className="font-sans font-bold text-xl">Danner</p>
+      </div>
+      <h1 className="text-3xl font-bold text-left py-7 px-3">Bull Run Chelsea</h1>
+      <div className="px-3 py-3">
+        <img src="https://s7d3.scene7.com/is/image/LaCrosse/danner_22_pdp/15481" alt="Bull Run Chelsea 6&quot; Brown Wedge" title="Bull Run Chelsea 6&quot; Brown Wedge"></img>
+      </div>
+      <div className="flex justify-center py-3">
+        <button className="px-24 py-3 outline rounded-md bg-blue-600 text-slate-100">Buy Now</button>
+      </div>
+    </>
   )
 }
